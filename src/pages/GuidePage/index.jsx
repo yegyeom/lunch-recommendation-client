d84a87837a78015b28343db166891242e665b0a3
yegyeom <reallyy1111@gmail.com>
2022-05-17 16:24:20 +0900

    [feat] 이용 가이드 페이지 구현

--- src/pages/GuidePage/index.jsx
+++ src/pages/GuidePage/index.jsx
@@ -1,5 +1,33 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import img1 from "../../assets/guide_1.png";
+import img2 from "../../assets/guide_2.png";
+import img3 from "../../assets/guide_3.png";
+
 const GuidePage = () => {
-  return <h1>이용 가이드</h1>;
+  const items = [img1, img2, img3];
+
+  const slides = items.map((item, idx) => (
+    <SwiperSlide key={idx} className="guide-slide">
+      <img className="guide-image" alt="guide-img" src={item}></img>
+    </SwiperSlide>
+  ));
+
+  return (
+    <div className="guide-swiper">
+      <Swiper
+        sliderPerView={3}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="slider"
+      >
+        {slides}
+      </Swiper>
+    </div>
+  );
 };
 
 export default GuidePage;
