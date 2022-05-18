0864aba40fdc01a72fd44044a9b75f3fab1c4ddf
yegyeom <reallyy1111@gmail.com>
2022-05-18 11:00:59 +0900

    [feat] 카테고리 아이템 추가

---
+++ src/utils/allCategoryItems.js
@@ -0,0 +1,10 @@
+export const getAllCategoryItems = () => {
+    const categoryItems = [
+        { index: 0, title: "전체" },
+        { index: 1, title: "질문" },
+        { index: 2, title: "추천" },
+        { index: 3, title: "자유" },
+    ];
+
+    return categoryItems
+};
---
+++ src/utils/categoryItems.js
@@ -0,0 +1,9 @@
+export const getCategoryItems = () => {
+    const categoryItems = [
+        { index: 1, title: "질문" },
+        { index: 2, title: "추천" },
+        { index: 3, title: "자유" },
+    ];
+
+    return categoryItems
+};
