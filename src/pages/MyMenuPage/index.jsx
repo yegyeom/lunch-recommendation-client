94ab8005d1be4891820ac46ba907de6c6fc38e1d
yegyeom <reallyy1111@gmail.com>
2022-05-18 11:04:25 +0900

    [refactor] 나의 메뉴 실제 유저 정보로 변경

--- src/pages/MyMenuPage/index.jsx
+++ src/pages/MyMenuPage/index.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { useState } from "react";
+import React, { useContext } from "react";
 import MenuHistory from "./MenuHistory";
+import { AuthContext } from "../../contexts/AuthContextProvider";
 
 const MyMenuPage = () => {
-  const [nickname, setNickname] = useState("동작구고독한미식가");
+  const { user } = useContext(AuthContext);
 
   return (
-    <div className="my-menu-container">
-      <div className="user-text-container">
-        <h2>
-          <span className="user-nickname">{nickname}</span>님이
-        </h2>
-        <h2>
-          선택하셨던 메뉴에요!
-        </h2>
+    <>
+      <div className="my-menu-container">
+        <div className="user-text-container">
+          <h2>
+            <span className="user-nickname">{user.profile.nickname}</span>
+            님이
+          </h2>
+          <h2>선택하셨던 메뉴에요!</h2>
+        </div>
+        <MenuHistory />
       </div>
-      <MenuHistory />
-    </div>
+    </>
   );
 };
 
